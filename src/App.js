403d6623130f0b9222bb1ba4adda99967e402701
Boyd Barker <boyd.barker@ggit.ca>
2023-02-14 16:33:46 -0800

    first test UI from chatGPT

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import React from 'react';
+import { Grid, Button } from '@material-ui/core';
 
-
-export default function App() {
+function App() {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <h1>What's New</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={6} sm={3}>
+          <Button variant="contained" color="primary" href="/exams">
+            Upcoming 7 exams
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="contained" color="primary" href="/homeworks">
+            Pending 16 homeworks
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="contained" color="primary" href="/notifications">
+            New 5 notifications
+          </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Button variant="contained" color="primary" href="/messages">
+            New 1 message
+          </Button>
+        </Grid>
+      </Grid>
+      <h1>Today</h1>
+      <ul>
+        <li>Meeting at 10:00 AM</li>
+        <li>Lunch at 12:30 PM</li>
+        <li>Workout at 5:00 PM</li>
+      </ul>
     </div>
   );
 }
+
+export default App;
